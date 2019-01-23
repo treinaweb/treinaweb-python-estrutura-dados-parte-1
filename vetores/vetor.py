647983cc071d5ed55d255d6edc2674211f533a42
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-01-23 16:03:00 -0300

    02.04_Inserindo_Elementos_Final

--- vetores/vetor.py
+++ vetores/vetor.py
@@ -1,12 +1,23 @@
 class Vetor():
     def __init__(self, tamanho):
         self.__tamanho = tamanho
-        self.__elementos = [None] * tamanho
+        self.__elementos = [None] * tamanho # [None], [None], [None]
+        self.__posicao = 0
 
     def inserir_elemento_posicao(self, elemento, posicao):
-        # 1, 2, 3
+        # 1, 2
         # inserir_elemento_posicao(3, 1)
         self.__elementos[posicao] = elemento
+
+    def inserir_elemento_final(self, elemento):
+        if self.__posicao >= len(self.__elementos):
+            # [None], [None], [None]
+            self.__elementos = self.__elementos + [None]
+            # [None], [None], [None], [None]
+        # 1, 2, 3
+        # 4
+        self.__elementos[self.__posicao] = elemento
+        self.__posicao += 1
 
     def listar_elemento(self, posicao):
         return self.__elementos[posicao]
