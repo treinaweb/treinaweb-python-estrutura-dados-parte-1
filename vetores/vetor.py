a39ecd10f5b47c9f2231b0be522126b713883253
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-01-23 16:33:40 -0300

    02.05_Inserindo_Elementos_Posicao

--- vetores/vetor.py
+++ vetores/vetor.py
@@ -5,9 +5,16 @@ class Vetor():
         self.__posicao = 0
 
     def inserir_elemento_posicao(self, elemento, posicao):
-        # 1, 2
-        # inserir_elemento_posicao(3, 1)
-        self.__elementos[posicao] = elemento
+        # 1, 2, 3
+        # 1, 2, 4, 3
+        # 1, 2, 4 vetor_inicio
+        # 3 vetor_final
+        # 2
+        vetor_inicio = self.__elementos[:posicao] + [None] #1, 2, [None]
+        vetor_final = self.__elementos[posicao:] #3
+        vetor_inicio[len(vetor_inicio) - 1] = elemento #1, 2, 4, 3
+        self.__elementos = vetor_inicio + vetor_final #1, 2, 4, 3
+        self.__posicao += 1
 
     def inserir_elemento_final(self, elemento):
         if self.__posicao >= len(self.__elementos):
