4eb09e477c4bc2313b4624d4cae8afdc08d234d2
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-01-29 15:31:20 -0300

    04.04_Removendo_Elementos_Lista_Duplamente_Ligada

--- listas/lista_duplamente_ligada.py
+++ listas/lista_duplamente_ligada.py
@@ -29,8 +29,8 @@ class ListaDuplamenteLigada():
             novo_no.anterior = self.__ultimo_no
             self.__ultimo_no = novo_no
         else:
-            no_anterior = self.recuperar_no(posicao - 1)
             no_atual = self.recuperar_no(posicao)
+            no_anterior = no_atual.anterior
             novo_no = NoDuplamenteLigada(elemento)
             no_anterior.proximo = novo_no
             novo_no.proximo = no_atual
@@ -87,15 +87,20 @@ class ListaDuplamenteLigada():
     def remover_posicao(self, posicao):
         if posicao == 0:
             proximo_no = self.__primeiro_no.proximo
-            self.__primeiro_no = None
+            self.__primeiro_no.proximo = None
+            proximo_no.anterior = None
             self.__primeiro_no = proximo_no
         elif posicao == self.__tamanho - 1:
-            penultimo_no = self.recuperar_no(self.__tamanho - 2)
+            penultimo_no = self.__ultimo_no.anterior
             penultimo_no.proximo = None
+            self.__ultimo_no.anterior = None
             self.__ultimo_no = penultimo_no
         else:
             no_remover = self.recuperar_no(posicao)
-            no_anterior = self.recuperar_no(posicao - 1)
-            no_anterior.proximo = no_remover.proximo
+            no_anterior = no_remover.anterior
+            no_proximo = no_remover.proximo
+            no_anterior.proximo = no_proximo
+            no_proximo.anterior = no_anterior
             no_remover.proximo = None
+            no_remover.anterior = None
         self.__tamanho -= 1
